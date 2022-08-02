976be7a4aa1c4ac8371bdc9f0c7c9b20d49d4b80
donsonioc2010 <whddnjs822@gmail.com>
2022-08-02 22:06:50 +0900

    Init Project Read YamlValueClass

---
+++ src/main/java/com/nooblol/global/config/RiotConfiguration.java
@@ -0,0 +1,21 @@
+package com.nooblol.global.config;
+
+import com.nooblol.global.utils.YamlLoadFactory;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource(value = "classpath:constants.yml", factory = YamlLoadFactory.class)
+@ConfigurationProperties(prefix = "riot")
+@Getter
+@Setter
+public class RiotConfiguration {
+
+  @Value("${riot.apiKey}")
+  private String apiKey;
+
+}
