a87f84decf0613c12e30b4c7ddba837b9fed42bb
donsonioc2010 <whddnjs822@gmail.com>
2022-08-19 05:13:06 +0900

    feat : 사용하는 Rune정보를 Response할 Dto추가

---
+++ src/main/java/com/nooblol/account/dto/match/MatchUseRuneDto.java
@@ -0,0 +1,18 @@
+package com.nooblol.account.dto.match;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class MatchUseRuneDto {
+
+  private String puuid;
+  private String matchId;
+  private String type;
+  private String sortNo;
+  private String perk;
+}
