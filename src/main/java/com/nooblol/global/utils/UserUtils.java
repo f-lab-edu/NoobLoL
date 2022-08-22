bdd23292d3099f0550019f74f72c471ddcdf066e
donsonioc2010 <whddnjs822@gmail.com>
2022-08-22 18:35:29 +0900

    feat : Enum, Utils추가
    - UserRole Enum을 통한 관리
    - 자주사용되는 ResponseDto상태들 Enum을 통한 관리
    - UserUtil을 통하여 패스워드 암호화 기능 구현

---
+++ src/main/java/com/nooblol/global/utils/UserUtils.java
@@ -0,0 +1,25 @@
+package com.nooblol.global.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class UserUtils {
+
+  /**
+   * 인수값 문자열을 SHA-512 로 암호화
+   *
+   * @param value 암호화를 진행할 문자열
+   * @return 암호화가 진행된 문자열
+   * @throws NoSuchAlgorithmException
+   * @throws UnsupportedEncodingException
+   */
+  public static String stringChangeToSha512(String value)
+      throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    MessageDigest md = MessageDigest.getInstance("SHA-512");
+    md.update(value.getBytes(StandardCharsets.UTF_8));
+    return Base64.getEncoder().encodeToString(md.digest());
+  }
+}
