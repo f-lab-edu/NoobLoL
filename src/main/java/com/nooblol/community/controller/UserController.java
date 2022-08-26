2dc4d21ffe8c9c4db8df3dc2d68b455048d4788a
donsonioc2010 <whddnjs822@gmail.com>
2022-08-26 21:08:46 +0900

    feat : 사용자 정보 수정기능 구현

--- src/main/java/com/nooblol/community/controller/UserController.java
+++ src/main/java/com/nooblol/community/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.nooblol.community.controller;
 
 import com.nooblol.community.dto.UserSignUpRequestDto;
+import com.nooblol.community.dto.UserInfoUpdateDto;
+import com.nooblol.community.service.UserInfoService;
 import com.nooblol.community.service.UserSignUpService;
 import com.nooblol.global.dto.ResponseDto;
 import javax.validation.Valid;
@@ -25,9 +27,24 @@ public class UserController {
 
   private final UserSignUpService userSignUpService;
 
+  private final UserInfoService userInfoService;
+
   @PostMapping("/singup")
   public ResponseDto singUpSubmit(@Valid @RequestBody UserSignUpRequestDto userSignUpDto) {
     return userSignUpService.signUpUser(userSignUpDto);
+  }
+
+
+  /**
+   * UserName과 UserPassword에 대해서만 수정이 가능하며, 관리자와 메일을 인증한 사용자만 사용자 정보에 대하여 변경이 가능하다.
+   *
+   * @param userInfoUpdateDto
+   * @return 정상적으로 Update가 성공했으면 OK Response와 결과값으로 true가 Return되며, 정보 수정에 실패하면 OK상태코드와 false값을
+   * Return한다
+   */
+  @PostMapping("/update")
+  public ResponseDto userUpdate(@Valid @RequestBody UserInfoUpdateDto userInfoUpdateDto) {
+    return userInfoService.updateUserInfo(userInfoUpdateDto);
   }
 
   /**
@@ -39,13 +56,14 @@ public class UserController {
   @GetMapping("/resend-authmail/{email:.+}")
   public ResponseDto resendAuthMail(
       @PathVariable @NotBlank @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
-          String email
+      String email
   ) {
     return userSignUpService.reSendSignUpUserMail(email.trim());
   }
 
   /**
    * UserId를 받아 비활성화가 진행된 사용자의 UserRole을 활성상태인 `UserRoleStatus.AUTH_USER`로 변경함
+   *
    * @param userId users테이블의 userId Value
    * @return 404 : userId의 정상적인 입력이 아님, 500 : DB처리 도중 문제 발생, OK true: 정상적인 변경, OK false : Update
    * Fail
@@ -54,5 +72,6 @@ public class UserController {
   public ResponseDto authUserByMail(@PathVariable @NotBlank String userId) {
     return userSignUpService.changeRoleAuthUser(userId.trim());
   }
+
 
 }
