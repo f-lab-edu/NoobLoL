f31394b7b5553f784ab7d267912436fccde30649
donsonioc2010 <whddnjs822@gmail.com>
2022-09-07 23:50:34 +0900

    refactor : slf4j로 어노테이션으로 수정

--- src/main/java/com/nooblol/community/controller/UserController.java
+++ src/main/java/com/nooblol/community/controller/UserController.java
@@ -8,8 +8,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,12 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
-
-  private final Logger log = LoggerFactory.getLogger(getClass());
 
   private final UserSignUpService userSignUpService;
 
