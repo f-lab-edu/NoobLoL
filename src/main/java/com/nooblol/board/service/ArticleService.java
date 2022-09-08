54a4223a7310543a29db8be71659ebcc1f8cb910
donsonioc2010 <whddnjs822@gmail.com>
2022-09-09 02:27:06 +0900

    docs : 주석수정

--- src/main/java/com/nooblol/board/service/ArticleService.java
+++ src/main/java/com/nooblol/board/service/ArticleService.java
@@ -30,9 +30,11 @@ public interface ArticleService {
   void addReadCount(int articleId);
 
   /**
-   * 게시물 Upsert
+   * 게시물의 삽입 또는 수정을 진행한다.
    *
    * @param articleDto
+   * @param session
+   * @param isInsert   삽입인 경우 True, Update인 경우 False
    * @return
    */
   boolean upsertArticle(ArticleDto articleDto, HttpSession session, boolean isInsert);
