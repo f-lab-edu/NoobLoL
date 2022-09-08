b82547374da4e870f4145d7a72f6c833711baf7b
donsonioc2010 <whddnjs822@gmail.com>
2022-09-08 16:26:48 +0900

    feat : 로그인이 안된경우 UNAUTH코드에 대한 Return추가 및 AOP 설정 추가

--- src/main/java/com/nooblol/global/utils/ResponseEnum.java
+++ src/main/java/com/nooblol/global/utils/ResponseEnum.java
@@ -10,8 +10,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ResponseEnum {
   BAD_REQUEST(HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST),
+
   NOT_FOUND(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND),
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR),
+
+  UNAUTHORIZED(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED),
+
   OK(HttpStatus.OK.value(), null),
   CONFLICT(HttpStatus.CONFLICT.value(), null);
 
