5486273bc4a21609b13e040e4069b232fb214da5
donsonioc2010 <whddnjs822@gmail.com>
2022-09-11 22:37:52 +0900

    feat : 상수화 추가

---
+++ src/main/java/com/nooblol/community/utils/AdminConstants.java
@@ -0,0 +1,11 @@
+package com.nooblol.community.utils;
+
+public class AdminConstants {
+
+  private AdminConstants() {
+  }
+
+  public static final String ADMIN_USERID_NULL = "사용자의 UserId가 입력되지 않았습니다.";
+
+  public static final String ADMIN_DELETEUSERID_NULL = "삭제하려는 사용자의 UserId가 입력되지 않았습니다.";
+}
