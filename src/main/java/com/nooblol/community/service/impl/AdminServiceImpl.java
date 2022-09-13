d00a77528c1146be8d55e380bec6a0129fc90f86
donsonioc2010 <whddnjs822@gmail.com>
2022-09-13 21:19:59 +0900

    refactor : 관리자 기능 AOP를 통한 사전 관리자 체크 기능 추가에 따른 소스 수정

--- src/main/java/com/nooblol/community/service/impl/AdminServiceImpl.java
+++ src/main/java/com/nooblol/community/service/impl/AdminServiceImpl.java
@@ -34,12 +34,7 @@ public class AdminServiceImpl implements AdminService {
   private final AdminMapper adminMapper;
 
   @Override
-  public ResponseDto addAdminMember(
-      UserSignUpRequestDto userSignUpRequestDto, HttpSession session
-  ) {
-    if (isSessionUserIsNotAdmin(session)) {
-      throw new IllegalArgumentException(ExceptionMessage.FORBIDDEN);
-    }
+  public ResponseDto addAdminMember(UserSignUpRequestDto userSignUpRequestDto) {
     try {
       String password = EncryptUtils.stringChangeToSha512(userSignUpRequestDto.getPassword());
       userSignUpRequestDto.setPassword(password);
@@ -59,41 +54,19 @@ public class AdminServiceImpl implements AdminService {
   }
 
   @Override
-  public ResponseDto deleteAdminMember(UserSignOutDto userSignOutDto, HttpSession session) {
-    if (isSessionUserIsNotAdmin(session)) {
-      throw new IllegalArgumentException(ExceptionMessage.FORBIDDEN);
-    }
+  public ResponseDto deleteAdminMember(UserSignOutDto userSignOutDto) {
     return userSignOutService.signOutUser(userSignOutDto);
   }
 
-  /*
-    TODO: [22. 09. 10] : 해당 부분에서 초기 생각은 요청자의 정보를 받아 DB에서 관리자가 맞는지 확인을 한 이후 작업하는 로직이었슴.
-    다시 생각해보니 HttpSession에서 정보를 가져온 이후 대조하는게 더 맞는 판단으로 보이나,
-    현재 Login정보가 담긴 기능이 제작되지 않은 상황. 로그인기능의 수정을 진행하면서 해당 영역의 수정이 필요함.
-   */
   @Override
-  public ResponseDto forceDeleteUser(String deleteUserId, HttpSession session) {
-    if (isSessionUserIsNotAdmin(session)) {
-      throw new IllegalArgumentException(ExceptionMessage.FORBIDDEN);
-    }
-
+  public ResponseDto forceDeleteUser(String deleteUserId) {
     ResponseDto resultDto = ResponseEnum.OK.getResponse();
     resultDto.setResult(adminMapper.forcedDeleteUser(deleteUserId) > 0);
     return resultDto;
   }
 
-  /*
-  TODO: [22. 09. 10] : 해당 부분에서 초기 생각은 요청자의 정보를 받아 DB에서 관리자가 맞는지 확인을 한 이후 작업하는 로직이었슴.
-  다시 생각해보니 HttpSession에서 정보를 가져온 이후 대조하는게 더 맞는 판단으로 보이나,
-  현재 Login정보가 담긴 기능이 제작되지 않은 상황. 로그인기능의 수정을 진행하면서 해당 영역의 수정이 필요함.
-
-  사용자의 강제삭제 기능과 똑같이 로직수정이 필요함
- */
   @Override
-  public ResponseDto getAllUserList(int pageNum, int limitNum, HttpSession session) {
-    if (isSessionUserIsNotAdmin(session)) {
-      throw new IllegalArgumentException(ExceptionMessage.FORBIDDEN);
-    }
+  public ResponseDto getAllUserList(int pageNum, int limitNum) {
     List<UserDto> userList = adminMapper.getAllUserList(pageNum, limitNum);
 
     ResponseDto resultDto = ResponseEnum.OK.getResponse();
@@ -103,14 +76,12 @@ public class AdminServiceImpl implements AdminService {
   }
 
   @Override
-  public ResponseDto changeToActiveUser(String changeUserId, HttpSession session) {
-    isSessionUserIsNotAdmin(session);
+  public ResponseDto changeToActiveUser(String changeUserId) {
     return updateUser(changeUserId, UserRoleStatus.AUTH_USER.getRoleValue());
   }
 
   @Override
-  public ResponseDto changeToSuspensionUser(String changeUserId, HttpSession session) {
-    isSessionUserIsNotAdmin(session);
+  public ResponseDto changeToSuspensionUser(String changeUserId) {
     return updateUser(changeUserId, UserRoleStatus.SUSPENSION_USER.getRoleValue());
   }
 
@@ -123,19 +94,5 @@ public class AdminServiceImpl implements AdminService {
     ResponseDto result = ResponseEnum.OK.getResponse();
     result.setResult(adminMapper.changeUserRole(changeUser) > 0);
     return result;
-  }
-
-  /**
-   * 현재 로그인 중인 사용자가 관리자인지의 확인
-   *
-   * @param session
-   * @return
-   */
-  private boolean isSessionUserIsNotAdmin(HttpSession session) {
-    /**
-     * TODO [22. 09. 10] : Session의 Login정보를 담는 기능이 이후에 개발되었다 보니 Git으로 가져오는 것보다는 로그인, 비로그인작업을 진행하면서 수정 예정
-     * TODO [22. 09 .13] : 로그인 로그아웃시 AOP로 변경할것.
-     */
-    return false;
   }
 }
