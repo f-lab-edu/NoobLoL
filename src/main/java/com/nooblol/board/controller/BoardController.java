5cec9ec92d81c1c47252714481dc447e0ed461d8
donsonioc2010 <whddnjs822@gmail.com>
2022-09-20 17:05:38 +0900

    feat : 게시판 조회에 대한 Mapping 수정

--- src/main/java/com/nooblol/board/controller/BoardController.java
+++ src/main/java/com/nooblol/board/controller/BoardController.java
@@ -3,14 +3,12 @@ package com.nooblol.board.controller;
 import com.nooblol.board.utils.BoardStatusEnum;
 import com.nooblol.global.dto.ResponseDto;
 import com.nooblol.board.service.CategoryService;
-import com.nooblol.global.exception.ExceptionMessage;
 import com.nooblol.global.utils.CommonUtils;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,20 +41,16 @@ public class BoardController {
    * @param status     희망하는 상태값을 받는다. 없는 경우 DefaultValue로 Active상태값이 주어진다.
    * @return
    */
-  @GetMapping({"/bbsList/{categoryId}/{status}", "/bbsList/{categoryId}", "/bbsList"})
+  @GetMapping("/bbsList")
   public ResponseDto getBbsList(
-      @PathVariable(required = false) Optional<Integer> categoryId,
-      @PathVariable(required = false) Optional<Integer> status
+      @RequestParam(value = "categoryId") int categoryId,
+      @RequestParam(value = "status", required = false) Integer status
   ) {
-    if (categoryId.isEmpty()) {
-      throw new IllegalArgumentException(ExceptionMessage.BAD_REQUEST);
+    if (ObjectUtils.isEmpty(status)) {
+      status = BoardStatusEnum.ACTIVE.getStatus();
     }
-
-    //객체가 빈경우 DefaultValue로 Active값
-    Integer statusValue = status.orElse(BoardStatusEnum.ACTIVE.getStatus()).intValue();
-    Integer categoryIdValue = categoryId.get().intValue();
     return CommonUtils.makeListToResponseDto(
-        categoryService.getBbsList(categoryIdValue, statusValue)
+        categoryService.getBbsList(categoryId, status)
     );
   }
 
